b2d0fc733dcb0aacb88faba8bd841e04948cc889
reimerei <git@bjrm.de>
2023-10-31 09:20:23 +0100

    fix app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Homeassistant.MixProject do
 
   def project do
     [
-      app: :homeassistant,
+      app: :ex_homeassistant,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
